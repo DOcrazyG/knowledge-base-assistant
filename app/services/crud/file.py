df9f58eced6e5c7cb6806806ff45f3274c2d1682
lyuuhao <117192@runjian.com>
2025-09-23 11:48:53 +0800

    create file when already existed

--- app/services/crud/file.py
+++ app/services/crud/file.py
@@ -7,15 +7,36 @@ from ...schemas import file as file_schemas
 from ...models import file as file_models
 
 
-async def create_file(db: AsyncSession, file_data: file_schemas.FileCreate):
+async def create_file(db: AsyncSession, create_data: file_schemas.FileCreate):
     """
     创建文件记录
     """
+    # 根据user_id和filename查找文件是否存在
+    result = await db.execute(
+        select(file_models.File)
+        .where(file_models.File.user_id == create_data.user_id)
+        .where(file_models.File.filename == create_data.filename)
+    )
+    exist_file = result.scalar_one_or_none()
+    if exist_file:
+        logger.debug(f"File already exists: {create_data.filename}")
+        db_file = await update_file(
+            db=db,
+            update_data=file_schemas.FileUpdate(
+                file_id=exist_file.id,
+                user_id=create_data.user_id,
+                filename=create_data.filename,
+                minio_path=create_data.minio_path,
+                size=create_data.size,
+            ),
+        )
+        return db_file
+    
     db_file = file_models.File(
-        user_id=file_data.user_id,
-        minio_path=file_data.minio_path,
-        filename=file_data.filename,
-        size=file_data.size,
+        user_id=create_data.user_id,
+        minio_path=create_data.minio_path,
+        filename=create_data.filename,
+        size=create_data.size,
     )
     try:
         db.add(db_file)
@@ -77,4 +98,28 @@ async def delete_file(db: AsyncSession, file_id: int):
     except Exception as exc:
         await db.rollback()
         logger.error(f"Error deleting file: {exc}")
+        raise exc
+
+
+async def update_file(db: AsyncSession, update_data: file_schemas.FileUpdate):
+    """
+    更新文件记录
+    """
+    result = await db.execute(
+        select(file_models.File).where(file_models.File.id == update_data.file_id)
+    )
+    db_file = result.scalar_one_or_none()
+
+    if not db_file:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="文件未找到")
+    update_data = update_data.model_dump(exclude_unset=True)
+    try:
+        for key, value in update_data.items():
+            setattr(db_file, key, value)
+        await db.commit()
+        await db.refresh(db_file)
+        return db_file
+    except Exception as exc:
+        await db.rollback()
+        logger.error(f"Error updating file: {exc}")
         raise exc
